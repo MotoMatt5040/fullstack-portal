0dc92d14129127e89d96672e522bbba7617b52bc
matt@aero <motomatt5040@gmail.com>
2025-02-19 13:15:07 -0600

    fix(proj): fix docker
    Fixed files to allow docker to run properly. Mock (self-signed) https has been tested.

--- client/config.js
+++ client/config.js
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: 'build',
+export default defineConfig({
+  build: {
+    outDir: 'build',
+  },
+  plugins: [react(), eslint()],
+  server: {
+    host: true,
+    port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': '/src',
     },
-    plugins: [react(), eslint()],
-    server: {
-      port: 3000,
-    }
-  };
+  },
 });
