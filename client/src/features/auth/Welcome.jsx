88a57c105aafddae996135c220c3e111270b7226
matt@aero <motomatt5040@gmail.com>
2025-02-26 14:32:52 -0600

    feat(welcome): add info/github issue
    Added some info and github issue / feature request posting

--- client/src/features/auth/Welcome.jsx
+++ client/src/features/auth/Welcome.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux"
 import { selectCurrentUser, selectCurrentToken, logOut } from "./authSlice"
 import { Link } from "react-router-dom"
+import IssueForm from "../Github";
 
 const Welcome = () => {
     const dispatch = useDispatch()
@@ -15,12 +16,20 @@ const Welcome = () => {
         dispatch(logOut())
     }
 
+    const handleReportOrRequest = () => {
+
+    }
+
     const content = (
         <section className="welcome">
             <h1>{welcome}</h1>
-            <p>Token: {tokenAbbr}</p>
+            {/* <p>Token: {tokenAbbr}</p> */}
+            <h2>This page is currently under development.</h2>
             <p><Link to="/userslist">Go to the Users List</Link></p>
             <button onClick={handleLogout}>Log Out</button>
+            <p>Need to report an issue or request a new feature?</p>
+            <button onClick={handleReportOrRequest}>Report/Request</button>
+            <IssueForm/>
         </section>
     )
 
