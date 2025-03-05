e9bed305419558926b4383fbd2065e02211fe290
matt@aero <motomatt5040@gmail.com>
2025-03-05 15:08:34 -0600

    feat(ui): add placeholder
    Added a placeholder file for future use

---
+++ client/src/features/pages/ProjectOverview.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const ProjectOverview = () => {
+
+  
+
+  return (
+    <div>ProjectOverview</div>
+  )
+}
+
+export default ProjectOverview
