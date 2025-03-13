ef6215b4ece673ff5731f4839ffa918d3006c0b3
matt@aero <motomatt5040@gmail.com>
2025-03-13 16:33:44 -0500

    feat(ui): add navbar
    Added a navbar

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import React, { useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
+import NavBar from './NavBar';
 
 const Layout = () => {
-    return <Outlet />;
+  const [isNavVisible, setIsNavVisible] = useState(false);
+  const location = useLocation();
+
+  // Check if the current route requires authentication
+  const isProtectedRoute = location.pathname !== '/login' && location.pathname !== '/reset-password';
+
+  const handleNavToggle = (visibility) => {
+    setIsNavVisible(visibility); // Update the navbar visibility state
+  };
+
+  return (
+    <section>
+      {/* Render NavBar only for protected routes */}
+      {isProtectedRoute && <NavBar onToggleNav={handleNavToggle} />}
+
+      {/* Adjust main content based on navbar visibility */}
+      <main className={isNavVisible ? 'navbar-visible' : ''}>
+        <Outlet />
+      </main>
+    </section>
+  );
 };
 
 export default Layout;
---
+++ client/src/views/settings/Settings.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { logOut } from '../../features/auth/authSlice';
+
+const settings = () => {
+  
+  const dispatch = useDispatch();
+	const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
+  
+
+	useEffect(() => {
+		document.documentElement.setAttribute('data-theme', theme);
+	}, [theme]);
+
+	const toggleTheme = () => {
+		const newTheme = theme === 'dark' ? 'light' : 'dark';
+		setTheme(newTheme);
+		localStorage.setItem('theme', newTheme);
+	};
+
+  const handleLogout = () => {
+    dispatch(logOut());
+}
+
+	return (
+		<section>
+      <button onClick={handleLogout}>Log Out</button>
+			<button className='theme-toggle' onClick={toggleTheme}>
+				Switch to {theme === 'dark' ? 'Light' : 'Dark'} Mode
+			</button>
+		</section>
+	);
+};
+
+export default settings;
