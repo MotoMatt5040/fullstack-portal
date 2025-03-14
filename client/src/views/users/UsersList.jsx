142ea9061b6ed16d2be17bf3c4472366b6dde203
matt@aero <motomatt5040@gmail.com>
2025-03-14 10:45:42 -0500

    style(ui/userlist): change formatting
    Changed the code formatting

--- client/src/views/users/UsersList.jsx
+++ client/src/views/users/UsersList.jsx
@@ -1,35 +1,35 @@
-import React from "react";
-import { useGetUsersQuery } from "../../features/users/usersApiSlice"
-import { Link } from "react-router-dom";
+import React from 'react';
+import { useGetUsersQuery } from '../../features/usersApiSlice';
+import { Link } from 'react-router-dom';
 
 const UsersList = () => {
-    const {
-        data: users,
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    } = useGetUsersQuery()
+	const {
+		data: users,
+		isLoading,
+		isSuccess,
+		isError,
+		error,
+	} = useGetUsersQuery();
 
-    let content;
-    if (isLoading) {
-        content = <p>"Loading..."</p>;
-    } else if (isSuccess) {
-        content = (
-            <section className="users">
-                <h1>Users List</h1>
-                <ul>
-                    {users.map((user, i) => {
-                        return <li key={i}>{user.Email}</li>
-                    })}
-                </ul>
-                <Link to="/welcome">Back to Welcome</Link>
-            </section>
-        )
-    } else if (isError) {
-        content = <p>{JSON.stringify(error)}</p>;
-    }
+	let content;
+	if (isLoading) {
+		content = <p>"Loading..."</p>;
+	} else if (isSuccess) {
+		content = (
+			<section className='users'>
+				<h1>Users List</h1>
+				<ul>
+					{users.map((user, i) => {
+						return <li key={i}>{user.Email}</li>;
+					})}
+				</ul>
+				<Link to='/welcome'>Back to Welcome</Link>
+			</section>
+		);
+	} else if (isError) {
+		content = <p>{JSON.stringify(error)}</p>;
+	}
 
-    return content
-}
-export default UsersList
+	return content;
+};
+export default UsersList;
