320233ec76546d4915602fc385a358d426a5071a
matt@aero <motomatt5040@gmail.com>
2025-03-17 13:10:00 -0500

    fix(live-update-ui): split all and location specific tables

--- client/src/views/live_projects/LiveProjects.jsx
+++ client/src/views/live_projects/LiveProjects.jsx
@@ -7,6 +7,7 @@ import {
 } from './liveProjectsApiSlice';
 import { useSearchParams, Link } from 'react-router-dom';
 import LiveProjectsFilters from './components/LiveProjectsFilters';
+import LiveProjectsSummaryTable from './components/LiveProjectsSummaryTable';
 
 const LiveProjects = () => {
 	// This helper is used to mitigate having to repeat the same logic for each query
@@ -15,7 +16,7 @@ const LiveProjects = () => {
 		const { data, refetch } = queryHook(params);
 		return { data, refetch };
 	};
-	
+
 	const [searchParams] = useSearchParams();
 
 	const projectidFromUrl = searchParams.get('projectid');
@@ -26,6 +27,9 @@ const LiveProjects = () => {
 
 	const [locations, setLocations] = useState([]);
 	const [projectids, setProjectids] = useState([]);
+
+	const [summaryData, setSummaryData] = useState([]);
+	const [detailData, setDetailData] = useState([]);
 
 	// These queries use the helper function to reduce the amount of code needed
 	// to be written for each query
@@ -59,10 +63,35 @@ const LiveProjects = () => {
 		}
 	}, [filteredLiveProjects.data]);
 
+	// useEffect(() => {
+	// 	// console.log(allLiveProjects.data)
+
+	// 	const filteredSummaryData = liveProjectData.data.filter(
+	// 		(project) => project.recloc === 99
+	// 	);
+	// 	// console.log(filteredSummaryData)
+	// 	setSummaryData(filteredSummaryData);
+
+	// 	const filteredDetailData = liveProjectData.data.filter(
+	// 		(project) => project.recloc !== 99
+	// 	);
+	// 	setDetailData(filteredDetailData);
+	// }, [liveProjectData]);
+
 	useEffect(() => {
 		allLiveProjects.refetch();
 		filteredLiveProjects.refetch();
 		liveProjectData.refetch();
+		const filteredSummaryData = liveProjectData.data.filter(
+			(project) => project.recloc === 99
+		);
+		// console.log(filteredSummaryData)
+		setSummaryData(filteredSummaryData);
+
+		const filteredDetailData = liveProjectData.data.filter(
+			(project) => project.recloc !== 99
+		);
+		setDetailData(filteredDetailData);
 	}, [projectid, location]);
 
 	let content = (
@@ -79,8 +108,11 @@ const LiveProjects = () => {
 			/>
 			<br />
 			<Link to='/welcome'>Back to Welcome</Link>
-			{liveProjectData.data && (
-				<LiveProjectsTable data={liveProjectData.data} />
+			{summaryData && (
+				<LiveProjectsSummaryTable data={summaryData} />
+			)}
+			{detailData && (
+				<LiveProjectsTable data={detailData} />
 			)}
 		</section>
 	);
---
+++ client/src/views/live_projects/components/LiveProjectsSummaryTable.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import '../liveProjectsTable.css';
+
+const LiveProjectsSummaryTable = ({ data }) => {
+  const handleCellClick = (eid) => {
+    console.log('cell: ', eid);
+  };
+
+  const handleRowClick = (eid) => {
+    console.log('row: ', eid);
+  };
+
+  return (
+    <section>
+      <table>
+        <thead>
+          <tr>
+            <th>Location</th>
+            <th>Project ID</th>
+            <th>Project Name</th>
+            <th>CPH</th>
+            <th>CMS</th>
+            <th>MPH</th>
+            <th>HRS</th>
+            <th>Avg. Len.</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((project, index) => (
+              <tr key={index}>
+                <td>{project.recloc === 99 ? "All" : project.recloc}</td>
+                <td>{project.projectid}</td>
+                <td>{project.projname}</td>
+                <td>{project.cph}</td>
+                <td>{project.cms}</td>
+                <td>{project.mph}</td>
+                <td>{project.hrs}</td>
+                <td>{project.al}</td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </section>
+  );
+};
+
+export default LiveProjectsSummaryTable;
