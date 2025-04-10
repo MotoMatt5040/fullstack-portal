1f3887305be769a3fc4a87b62ee72e81bee23858
matt@aero <motomatt5040@gmail.com>
2025-04-10 11:35:30 -0500

    feat(ui/table): add color
    Added coloring that uses thresholds to determine style

--- client/src/components/MyTable.jsx
+++ client/src/components/MyTable.jsx
@@ -1,35 +1,64 @@
 import React from 'react';
 import './css/MyTable.css';
 
-const MyTable = ({className = 'MyTable', data, columnKeyMap}) => {
+const MyTable = ({ className = 'MyTable', data, columnKeyMap, reverseThresholds = ['offCph'] }) => {
 
-	const columnHeaders = Object.keys(columnKeyMap);
+  const columnHeaders = Object.keys(columnKeyMap);
 
-	return (
-		<section>
-			<table className={className}>
-				<thead>
+  const highlightCellColor = (cellValue, threshold, columnName) => {
+    const isReversed = reverseThresholds.includes(columnName);
+
+    if (isReversed) {
+      if (cellValue > threshold * 1.1) {
+        return 'highlight-red'; 
+      } else if (cellValue > threshold * 0.95) {
+        return 'highlight-yellow'; 
+      } else if (cellValue < threshold * 0.9) {
+        return 'highlight-green'; 
+      }
+    } else {
+      if (cellValue < threshold * 0.9) { 
+        return 'highlight-red'; 
+      } else if (cellValue < threshold * 0.95) { 
+        return 'highlight-yellow'; 
+      } else if (cellValue > threshold * 1.1) { 
+        return 'highlight-green';
+      }
+    }
+    return '';
+  };
+
+  return (
+    <table className={className}>
+      <thead>
         <tr>
-            {columnHeaders.map((header) => (
-              <th key={header}>{header}</th>
-            ))}
-          </tr>
-				</thead>
-				<tbody>
-					{data.map((row, index) => (
-						<tr key={index}>
-							{columnHeaders.map((header) => {
-								const key = columnKeyMap[header]; 
-								return (
-									<td key={header}>{row?.[key] ?? 'N/A'}</td>
-								);
-							})}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</section>
-	);
+          {columnHeaders.map((header) => (
+            <th key={header}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => {
+          return (
+            <tr key={index}>
+              {columnHeaders.map((header) => {
+                const key = columnKeyMap[header]; 
+                const cellValue = row?.[key] ?? 'N/A';
+                const thresholdKey = `${columnKeyMap[header]}Threshold`;  
+                const threshold = row?.[thresholdKey]; 
+                const cellClass = threshold ? highlightCellColor(cellValue, threshold, columnKeyMap[header]) : '';
+                return (
+                  <td className={`${header} ${cellClass}`} key={header}>
+                    {cellValue}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default MyTable;
