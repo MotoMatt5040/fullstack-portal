ccaebd33072bf41f224f294c69ab8e6d9e3b64aa
matt@aero <motomatt5040@gmail.com>
2025-04-10 11:28:06 -0500

    refactor(proj): move/del/change
    Files moved, deleted, or changed name

--- client/src/views/project_report/useProjectReportLogic.jsx
+++
@@ -1,8 +0,0 @@
-import { useState, useEffect } from 'react';
-import { parseDate } from '@internationalized/date';
-
-const useProjectReportLogic = () => {
-  const [projectIds, setProjectIds] = useState([]);
-}
-
-export default useProjectReportLogic;
