d49d020adcb1d1f1a32c6a02e22451da9d86259e
matt@aero <motomatt5040@gmail.com>
2025-04-10 11:38:33 -0500

    refactor(ui): moved
    files moved

---
+++ client/src/views/Public.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom"
+
+const Public = () => {
+
+    const content = (
+        <section className="public">
+            <header>
+                <h1>Welcome to the Promark Portal</h1>
+            </header>
+            <main>
+                {/* <p>Currently you can log in, and do nothing else.</p>
+                <p>&nbsp;</p> */}
+                <address>
+                    Promark Research<br />
+                    313 Sawdut Rd<br />
+                    Spring, TX 77380<br />
+                    <a href="tel:+15555555555">(555) 555-5555</a>
+                </address>
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+            </footer>
+        </section>
+
+    )
+    return content
+}
+export default Public
---
+++ client/src/views/login/Login.jsx
@@ -0,0 +1,118 @@
+import { useRef, useState, useEffect } from "react";
+
+import { useDispatch } from "react-redux";
+import React from "react";
+import { setCredentials } from "../../features/auth/authSlice";
+import { useLoginMutation } from "../../features/auth/authApiSlice";
+
+import useInput from "../../hooks/useInput";
+import useToggle from "../../hooks/useToggle";
+
+import { Link } from "react-router-dom";
+
+import "./Login.css";
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [user, setUser, userAttribs] = useInput("user", "");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [check, toggleCheck] = useToggle("persist", false);
+
+  const [login, { isLoading }] = useLoginMutation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const userData = await login({ user, pwd }).unwrap();
+      dispatch(setCredentials({ ...userData, user }));
+      document.cookie = `persist=${check}; path=/`;
+      setUser("");
+      setPwd("");
+    } catch (err) {
+      if (!err?.status) {
+        setErrMsg("No Server Response");
+      } else if (err.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.status === 401) {
+        setErrMsg("Unauthorized");
+      } else {
+        setErrMsg("Login Failed");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handlePwdInput = (e) => setPwd(e.target.value);
+
+  const content = isLoading ? (
+    <>
+      <h1>Loading...</h1>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+    </>
+  ) : (
+    <section className="login">
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          {...userAttribs}
+          required
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={handlePwdInput}
+          value={pwd}
+          required
+        />
+        <button>Sign In</button>
+        <div className="persistCheck">
+          <input
+            type="checkbox"
+            id="persist"
+            onChange={toggleCheck}
+            checked={check}
+          />
+          <label htmlFor="persist">&nbsp;Keep me logged in.</label>
+        </div>
+        <div className="forgot-password-link">
+        <Link to="/reset-password">Forgot Password?</Link>
+      </div>
+      </form>
+      
+    </section>
+  );
+
+  return content;
+};
+export default Login;
