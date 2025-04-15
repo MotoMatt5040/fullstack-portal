f231fdd1b8826a747ae535c5d5843a009458fd2a
matt@aero <motomatt5040@gmail.com>
2025-04-15 10:46:21 -0500

    fix(ui): rem links
    Commented out links i only want visible for dev purposes

--- client/src/views/Welcome.jsx
+++ client/src/views/Welcome.jsx
@@ -22,12 +22,16 @@ const Welcome = () => {
             <h1>{welcome}</h1>
             <h2>This page is currently under development.</h2>
             
-            {roles.includes(ROLES.Admin) && <p><Link to="/userslist">Go to the Users List</Link></p>}
-            {roles.includes(ROLES.Admin) && <p><Link to="/updateuserroles">Update user roles</Link></p>}
+            {/* {roles.includes(ROLES.Admin) && <p><Link to="/userslist">Go to the Users List</Link></p>} */}
+            {/* {roles.includes(ROLES.Admin) && <p><Link to="/updateuserroles">Update user roles</Link></p>} */}
             
             <p><Link to="/github">Need to report an issue or request a new feature?</Link></p>
-            <p><Link to="/liveprojects">Live Projects Table</Link></p>
-            <p><Link to="/productionreport">Production Reports</Link></p>
+            {/* <p><Link to="/liveprojects">Live Projects Table</Link></p> */}
+            {/* <p><Link to="/productionreport">Production Reports</Link></p> */}
+            <br />
+            <br />
+            <br />
+            <h1>Start Here!!</h1>
             <p><Link to="/summaryreport">Summary Reports</Link></p>
             
         </section>
