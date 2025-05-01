709b751386ee5cc5c6b3f1b4a4a5b3dc49800ac7
matt@aero <motomatt5040@gmail.com>
2025-05-01 16:04:51 -0500

    fix(server): add roles, rem unused files
    Added roles and removed files that are no longer in use

--- server/config/rolesList.js
+++ server/config/rolesList.js
@@ -1,9 +1,9 @@
 const ROLES_LIST = {
     "Admin": 1,
-    "Manager": 2,
-    "External": 3,
-    "Team Lead": 4,
-    "Executive": 5,
+    "Executive": 2,
+    "Manager": 3,
+    "External": 4,
+    "Team Lead": 5,
     "Monitor": 6
 }
 
