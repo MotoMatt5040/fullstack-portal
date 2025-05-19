2fac134b06dd0c7118108f82838516e1324cac92
matt@aero <motomatt5040@gmail.com>
2025-05-19 10:22:50 -0500

    fix(server/quota): fix log
    Fixed console logs to not print out such a verbose error report.

--- server/services/QuotaServices.js
+++ server/services/QuotaServices.js
@@ -8,12 +8,13 @@ const getWebQuotas = async (sid) => {
 		const res = await axios.get(`/api/quotas/${sid}`);
 		return res.data;
 	} catch (error) {
-		console.error('Error fetching web quotas:', error);
+		console.error('Error fetching web quotas:');
 	}
 };
 
 const getPhoneQuotas = async (sid, token) => {
 	//sid is a voxco project id
+	// console.log(sid)
 	try {
 		const res = await axios.get(
 			`/voxco.api/projects/${sid}/stratas?status=All`,
@@ -25,7 +26,7 @@ const getPhoneQuotas = async (sid, token) => {
 		);
 		return res.data;
 	} catch (error) {
-		console.error('Error fetching phone quotas:', error);
+		console.error('Error fetching phone quotas:');
 	}
 };
 
