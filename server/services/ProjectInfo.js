aea7df0f7e4cbf96ce586538da00af5ffbd2a600
matt@aero <motomatt5040@gmail.com>
2025-06-12 11:47:07 -0500

    fix(api/proj-info): fix qry
    Fixed a query for getting the projects from voxco. This structures it so that the com file is displayed first for ordering purposes.

--- server/services/ProjectInfo.js
+++ server/services/ProjectInfo.js
@@ -22,7 +22,7 @@ const getPhoneProjects = async (projectId) => {
 				.request()
 				.input('projectId', sql.NVarChar, projectId)
 				.query( // ParentId > 1000 is to filter out folders. To understand run SELECT k_Id, name, * FROM [VoxcoSystem].[dbo].[tblObjects] where tblobjects.Type = 1 and name LIKE '13100%'
-					"SELECT k_Id, name FROM [VoxcoSystem].[dbo].[tblObjects] where tblobjects.Type = 1 and name LIKE @projectId + '%' and ParentId > 1000"
+					"SELECT k_Id, name FROM [VoxcoSystem].[dbo].[tblObjects] WHERE tblobjects.Type = 1 AND name LIKE @projectId + '%' and ParentId > 1000 ORDER BY name DESC"
 				);
 			return result.recordset;
 		},
